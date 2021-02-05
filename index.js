2d59097740b03aeef2a8ff20cc21516563cd56a1
kolton thompson <koltonthompson@koltons-MacBook-Pro.local>
2021-02-04 22:24:40 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// const button1 = document.getElementById('button1')
+// const button2 = document.getElementById('button2')
+// const button3 = document.getElementById('button3')
+// const button4 = document.getElementById('button4')
+// const button5 = document.getElementById('button5')
+// const button6 = document.getElementById('button6')
+// const button7 = document.getElementById('button7')
+
+
+// function markComplete1(){
+//     button1.style.backgroundColor = "lightBlue"    
+// }
+
+// function markComplete2(){
+//     button2.style.backgroundColor = "lightBlue"    
+// }
+
+// function markComplete3(){
+//     button3.style.backgroundColor = "lightBlue"    
+// }
+
+// function markComplete4(){
+//     button4.style.backgroundColor = "lightBlue"    
+// }
+
+// function markComplete5(){
+//     button5.style.backgroundColor = "lightBlue"    
+// }
+
+// function markComplete6(){
+//     button6.style.backgroundColor = "lightBlue"    
+// }
+
+// function markComplete7(){
+//     button7.style.backgroundColor = "lightBlue"    
+// }
+
+let completedButtons = document.querySelectorAll(["data-number"]); 
+console.log(completedButtons);
+
+// buttonArray.addEventListener('click', markComplete);
+
+
+// console.log(buttonArray)
+  
+// function markComplete(index) {
+//     buttonArray[index].style.backgroundColor = "lightBlue"
+// }
+
